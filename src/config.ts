b5e543488c883eb5d5905485265658d19317f012
Weston Selleck <wwselleck@gmail.com>
2022-05-30 00:02:03 -0500

    Remove old soda page

--- src/config.ts
+++ src/config.ts
@@ -53,14 +53,6 @@ export const load = async () => {
         display: 'Soda',
         sheetId: process.env.LISTS_SHEET_ID,
         sheetName: 'Soda',
-        listType: 'scored',
-        description: "Soda is very bad for you and you shouldn't drink it, but on occasion I do and these are how much I like the different kinds."
-      },
-      {
-        id: 'soda2',
-        display: 'Soda2',
-        sheetId: process.env.LISTS_SHEET_ID,
-        sheetName: 'Soda',
         listType: 'tiered',
         description: "Soda is very bad for you and you shouldn't drink it, but on occasion I do and these are how much I like the different kinds.",
         scale: [
