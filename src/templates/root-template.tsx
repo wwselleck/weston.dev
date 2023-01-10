a0a3e2ceb74c03296e73e2270065c55510fea4e6
Weston Selleck <wwselleck@gmail.com>
2023-01-09 20:50:55 -0600

    favicon

--- src/templates/root-template.tsx
+++ src/templates/root-template.tsx
@@ -13,6 +13,7 @@ export function renderRoot(node: React.ReactNode, opts?: Opts) {
       <head>
         <title>weston.dev ${subtitle} </title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="/public/favicon.png">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400;500;600;700&display=swap">
