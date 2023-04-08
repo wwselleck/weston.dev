06dc3615a13848387572e9cba2ee17ecbe7a5458
Weston Selleck <wwselleck@gmail.com>
2023-04-08 00:52:02 -0500

    split keyboard page

--- convert-to-webp.sh
+++ convert-to-webp.sh
@@ -2,6 +2,7 @@
 #
 shopt -s extglob
 shopt -s nullglob
-for i in *.{png,jpg,jpeg}; do
+for i in *.{png,jpg,jpeg,PNG}; do
     cwebp "$i" -o "${i%.*}".webp
 done
+rm -rf *.{png,jpg,jpeg,PNG}
--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -3,7 +3,7 @@ import { Nav } from "./Nav";
 
 export const PageWrapper: React.FC = ({ children }) => {
   return (
-    <div className="w-1/2 mx-auto my-32">
+    <div className="w-1/2 min-w-[860px] mx-auto my-32">
       <div className="mb-32">
         <Nav />
       </div>
